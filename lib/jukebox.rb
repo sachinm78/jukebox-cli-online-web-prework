f7f3f10bef01a5d2787c8775b8053fb394e7fa5d
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 20:55:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -47,24 +47,24 @@ end
 def run 
   command = "" 
   while command  
-  puts "Please enter a command:"
+    puts "Please enter a command:"
   command = gets.downcase.strip  
   case command 
     when 'list'
       list(songs)
-      when 'play'
-        list(songs)
-        play(songs)
-      when 'help'
-        help 
-      when 'exit'
-        exit_jukebox
-        break 
-      else 
-        help 
-      end 
+    when 'play'
+      list(songs)
+      play(songs)
+    when 'help'
+      help 
+    when 'exit'
+      exit_jukebox
+      break 
+    else 
+      help 
     end 
   end 
+end 
 
 
 
