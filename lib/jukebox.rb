aca2f9ff577a98b1e163a62eda10184bd5e61c4b
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 20:35:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,15 +21,8 @@ end
 
 
 def list(songs)
-  puts "1. Phoenix - 1901
-2. Tokyo Police Club - Wait Up
-3. Sufjan Stevens - Too Much
-4. The Naked and the Famous - Young Blood
-5. (Far From) Home - Tiga
-6. The Cults - Abducted
-7. Phoenix - Consolation Prizes
-8. Harry Chapin - Cats in the Cradle
-9. Amos Lee - Keep It Loose, Keep It Tight"
+  
+end
 
 
 def play
