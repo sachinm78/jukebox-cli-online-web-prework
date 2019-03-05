cd07014b118332ff88f9026af4670b005f3c3e19
Sachin Mahajan <sachinm78@gmail.com>
2019-03-05 20:43:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,10 +27,18 @@ def list(songs)
 end
 
 
-def play
-  
-end
-
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_request = gets.chomp
+  songs.each_with_index do |song, i|
+    if user_request.to_i == i
+      puts "playing #{songs[i-1]}"
+    elsif user_request == song 
+      puts "playing #{song[i]}"
+    else puts "Invalid input, please try again"
+    end
+  end
+end 
 
 
 
